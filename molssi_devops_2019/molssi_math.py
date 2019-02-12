2c31f556ade5e81add5b3cb19eb6227f7c21451f
alexholehouse <alex.holehouse@gmail.com>
2019-02-12 11:05:45 -0500

    Modified our mathe function

--- molssi_devops_2019/molssi_math.py
+++ molssi_devops_2019/molssi_math.py
@@ -5,6 +5,50 @@ A sample repo for the workshop what we did in january 2019 at VT
 Handles the primary functions
 """
 
+import numbers
+
+
+def mean(num_list):
+    """
+    Computes the mean of a list of numbers.
+
+    Parameters
+    -------
+    num_list : iterable of numerical values
+       List of numerical values
+
+    Returns
+    -------
+    mean_val : float
+       Average value (geometric mean)
+
+
+
+    """
+
+    # check list - this is restrictive, chec
+    #if not isinstance(num_list, list):
+    #    raise TypeError('Invalid input [%s] - input must be a list'%(num_list))
+
+    # check you can iterate through (gives set/numpy compatibility)
+    try:
+        some_object_iterator = iter(num_list)
+    except TypeError as te:
+        raise TypeError('Invalid input [%s] - input must be iterable' % (num_list))
+
+    # check
+    if len(num_list) == 0:
+        raise ZeroDivisionError('Cannot calculate meean of empty list')
+
+    total = 0
+    for i in num_list:
+        if not isinstance(i, numbers.Number):
+            raise TypeError('One or more of values in iterable was not a number')
+
+        total = total + i
+
+    return total / len(num_list)
+
 
 def canvas(with_attribution=True):
     """
