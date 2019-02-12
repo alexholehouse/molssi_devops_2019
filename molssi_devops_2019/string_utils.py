fb5354d4f18ac54e0bac6a5c456c933b0a15a519
Fang Liu <fangliu.ustc@gmail.com>
2019-02-12 11:51:19 -0500

    add title_case function to string_util

--- molssi_devops_2019/string_utils.py
+++ molssi_devops_2019/string_utils.py
@@ -9,4 +9,24 @@ Misc. string processing functions
 
 
 def title_case(sentence):
-    return True
+    """
+    Convert a string to title title_caseself.
+
+    Title case means that the first character of every word is capitalize lowercaseself.
+
+    Parameters
+    ----
+    sentences: string_utilsString to be converted to title case
+    """
+    if len(sentence)==0:
+            return ""
+    if isinstance(sentence,str):
+        raise TypeError("Input %s is not valid. string  type input is expected" %(sentence))
+
+    newsentence=sentence[0].upper()
+    for i in range(1,len(sentence)):
+        if sentence[i-1].isspace():
+            newsentence+=sentence[i].upper()
+        else:
+            newsentence+=sentence[i].lower()
+    return newsentence
